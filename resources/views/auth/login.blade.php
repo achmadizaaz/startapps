db445248569d03bf42c391aadad884e3599ac565
achmadizaaz <myachmadizaaz@gmail.com>
2023-03-01 12:47:07 +0700

    Edit Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,51 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.guest')
+@section('content')
+<section class="vh-100" style="background-color: #343232;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+              <div class="col-md-6 col-lg-5 d-none d-md-block">
+                <img src="{{ asset('/img/login.webp') }}"
+                  alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+              </div>
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
+  
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                    <div class="d-flex align-items-center mb-5 pb-1">
+                      <img src="{{ asset('/img/Universitas Merdeka Pasuruan.webp') }}" alt="" width="130px" height="130px">
+                      <span class="h2 fw-bold mb-0 text-center" style="color:#1d1819">UNIVERSITAS MERDEKA PASURUAN</span>
+                    </div>
+                    <div class="text-center">
+                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;color:#444">Masuk dengan akun Anda</h5>
+                    </div>
+  
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="email">Email address</label>
+                      <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control form-control-lg" placeholder="Masukkan akun pengguna" required/>
+                    </div>
+  
+                    <div class="form-outline mb-4">
+                        <label class="form-label" for="password">Password</label>
+                      <input type="password" id="password"  name="password"
+                       class="form-control form-control-lg" placeholder="Masukkan katasandi" required autocomplete="current-password"/>
+                    </div>
+  
+                    <div class="pt-1 mb-4">
+                      <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                    </div>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                  </form>
+  
+                </div>
+              </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
